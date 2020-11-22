eb21a981e4f8cc340688ceeab15d197d125604d1
Belkacem Berchiche <b_berchiche@esi.dz>
2020-11-22 23:11:22 +0100

    Built first dataframe

--- get_tracks.py
+++ get_tracks.py
@@ -16,6 +16,15 @@ artist_ids = [
     "4ZzMtjQsjtaAOm3GPqmjeQ"
 ]
 
+artist_map = {
+    "11E9GHIAzJRKuECEUSEuqh": "Cheb Bello",  
+    "3e3cKwH1kUr02bvIm7VaIe": "Kader Japonais",
+    "5CZ5sGdn0X47HhndSYKqdz": "Cheba Dalila",
+    "0c3dDCJfxcT4lYNugbKvJt": "Warda",
+    "4iCrZzxACYPYcoS71DgjWW": "Bilal Sghir",
+    "59N7N5tX53jyPhAmsRi4or": "Cheb Bilal",
+    "4ZzMtjQsjtaAOm3GPqmjeQ" : "Cheb Djalil"
+}
 # 1.5 - Get basic artist info (Monthly listeners for instance)
 selected_artists = spotify.artists(artist_ids).get("artists")
 
@@ -23,9 +32,18 @@ selected_artists = spotify.artists(artist_ids).get("artists")
 top_tracks = list()
 for artist_id in artist_ids:
     artist_top_tracks = spotify.artist_top_tracks(artist_id).get("tracks")
+    for artist_top_track in artist_top_tracks: 
+        artist_top_track["main_artist"] = artist_map.get(artist_id)
+
     top_tracks.extend(artist_top_tracks)
 
-print(top_tracks)
+
 # 3 - Get maximum track features
+def get_track_id(track):
+    return track.get("id")
+
+tracks_ids = list(map(get_track_id, top_tracks))
+audio_features = spotify.audio_features(tracks=tracks_ids)
+
 # 4 - Store the data in a CSV file
 # 5 - Create a notebook and explore the data
