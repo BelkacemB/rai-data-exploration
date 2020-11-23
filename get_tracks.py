12c8034d65037b9247aba0822d9aad69b0e9a405
Belkacem Berchiche <b_berchiche@esi.dz>
2020-11-23 16:24:47 +0100

    Fetch youtube views

--- get_tracks.py
+++ get_tracks.py
@@ -1,7 +1,9 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import itertools
-
+import requests
+import json
+import urllib.parse
 # Do not forget so space out the requests
 spotify = spotipy.Spotify(
     client_credentials_manager=SpotifyClientCredentials())
@@ -24,6 +26,13 @@ selected_artists = spotify.artists(artist_map.keys()).get("artists")
 def get_track_id(track):
     return track.get("id")
 
+def get_youtube_views(search):
+    url = "http://youtube-scrape.herokuapp.com/api/search?q=" + urllib.parse.quote(search)
+    json_response = requests.get(url)
+    dict_response = json.loads(json_response.text)
+    views_text = dict_response["results"][0]["video"]["views"]
+    result = int(views_text[:-6].replace(",",""))
+    return result 
 
 all_tracks = list()
 for artist_id in artist_map.keys():
@@ -32,7 +41,9 @@ for artist_id in artist_map.keys():
         album_tracks = spotify.album_tracks(album.get("id")).get("items")
         full_tracks = spotify.tracks(list(map(get_track_id, album_tracks)))
         for track in full_tracks.get("tracks"):
-            track["main_artist"] = artist_map.get(artist_id)
+            artist_name = artist_map.get(artist_id)
+            track["main_artist"] = artist_name
+            track["youtube_views"] = get_youtube_views(track["name"] + " " + artist_name )
             all_tracks.append(track)
 
 # 3 - Get maximum track features
@@ -52,4 +63,3 @@ audio_features = list()
 for track_ids_chunk in chunked_iterable(tracks_ids, 99):
     audio_features.extend(spotify.audio_features(tracks=track_ids_chunk))
 
-print(len(audio_features))
